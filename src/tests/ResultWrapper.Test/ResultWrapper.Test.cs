2c464590367359bd118fa9b43fb46b0e9a0a4e9c
0605abmu <0605AbMu@gmail.com>
2024-05-05 11:48:20 +0500

    wrapper id covered by unit tests

---
+++ src/tests/ResultWrapper.Test/ResultWrapper.Test.cs
@@ -0,0 +1,118 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ResultWrapper.Library;
+using ResultWrapper.Library.Interfaces;
+
+namespace ResultWrapper.Test;
+
+public class ResultWrapperTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Id_ShouldBeNewGuid_WhenAnyResultSet()
+    {
+        Wrapper wrapper = ("test", 200);
+        
+        Assert.IsInstanceOf<Guid>(wrapper.Id);
+        Assert.That(wrapper.Id, Is.Not.EqualTo(Guid.Empty));
+    }
+    
+    [Test]
+    public void ResultContent_ShouldBeStringObject_WhenGiveOnlyStringObject()
+    {
+        string result = "test";
+
+        Wrapper wrapper = result;
+
+        Assert.That(wrapper.Content, Is.EqualTo(result));
+    }
+
+    [Test]
+    public void ResultContent_ShouldBeStringObject_And_StatusCode_ShouldBeSuccess_WhenGiveStringObjectWith200Code()
+    {
+        string result = "test";
+
+        Wrapper wrapper = (result, 200);
+
+        Assert.That(wrapper.Content, Is.EqualTo(result));
+        Assert.That(wrapper.Code, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    public void Error_ShouldNotBeNull_WhenExceptionGives()
+    {
+        var exceptionMessage = "Test exception";
+        var exception = new Exception(exceptionMessage);
+
+        Wrapper wrapper = exception;
+
+        Assert.NotNull(wrapper.Error);
+    }
+
+    [Test]
+    public void Content_ShouldBeNull_WhenExceptionGives()
+    {
+        var exceptionMessage = "Test exception";
+        var exception = new Exception(exceptionMessage);
+
+        Wrapper wrapper = exception;
+
+        Assert.Null(wrapper.Content);
+    }
+
+    [Test]
+    public void Error_ShouldBeErrorMessage_WhenExceptionGives()
+    {
+        var exceptionMessage = "Test exception";
+        var exception = new Exception(exceptionMessage);
+
+        Wrapper wrapper = exception;
+
+        Assert.That(wrapper.Error, Is.EqualTo(exceptionMessage));
+    }
+
+    [Test]
+    [TestCase(200, HttpStatusCode.OK)]
+    [TestCase(400, HttpStatusCode.BadRequest)]
+    [TestCase(404, HttpStatusCode.NotFound)]
+    [TestCase(500, HttpStatusCode.InternalServerError)]
+    [TestCase(600, 600)]
+    public void Code_ShouldBeHttpStatusCode_WhenCodeGives(int code, HttpStatusCode statusCode)
+    {
+        Wrapper wrapper = code;
+
+        Assert.That(wrapper.Code, Is.EqualTo(statusCode));
+    }
+
+    [Test]
+    public void ModelStateError_ShouldNotBeNullAndEmpty_WhenModelStateDictionaryGives()
+    {
+        ModelStateDictionary dictionary = new ModelStateDictionary();
+        dictionary.AddModelError("username", "Min length must be 4");
+
+        IWrapperGeneric<object> wrapper = Wrapper.ResultFromModelState(dictionary);
+
+        Assert.That(wrapper.ModelStateError, Is.Not.Null);
+        Assert.IsNotEmpty(wrapper.ModelStateError!);
+    }
+
+    [Test]
+    public void ModelStateError_ShouldBeCollectionOfBindingErrors_WhenModelStateDictionaryGives()
+    {
+        ModelStateDictionary dictionary = new ModelStateDictionary();
+        dictionary.AddModelError("username", "Min length must be 4");
+
+        IWrapperGeneric<object> wrapper = Wrapper.ResultFromModelState(dictionary);
+        
+        Assert.IsNotNull(wrapper.ModelStateError);
+
+        var stateError = wrapper.ModelStateError![0];
+
+        Assert.That(stateError.Key, Is.EqualTo("username"));
+        Assert.That(stateError.ErrorMessage, Is.EqualTo("Min length must be 4"));
+    }
+}
